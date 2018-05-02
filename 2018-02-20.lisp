8de920023a80696ca6363bbfdd86954db614dd11
AlexElizalde <alexelizalde@my.unt.edu>
2018-05-02 11:41:33 -0500

    changed description in 02-20.lisp
    Was confusing to read, so I tried to make it more clear.

--- 2018-02-20.lisp
+++ 2018-02-20.lisp
@@ -1,8 +1,11 @@
 
-;;the program works by dividing the decimal number by 62, it stores the mod of that in a list called list-of-digits-for-solution
-;;if the quotient is not zero then the program stores the mod and passes the quotient along with the list to be divided again
-;;if the quotient is zero then the program is done and it prints the list
-    ;the list has to be reversed before the whole number is returned because it stores the digits in reversed order
+; The program divides the decimal number by 62 until the quotient is zero, 
+;It stores the mod of each divide operation  in a list called list-of-digits-for-solution
+
+;If the quotient is not zero then the program adds the mod to list-of-digits-for-solution
+;and then passes the quotient along with the list to be divided again.
+;If the quotient is zero then the program is done and it prints the list.
+;The list has to be reversed before the result is returned
 
 (defparameter *base62* (string "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
@@ -12,7 +15,7 @@
 	(remainder (mod input 62)))                               ; mod always returns integers
 
     (cond ((= quotient 0)               ;if the quotient is zero then the program is done. 
-	     (reverse (cons (subseq *base62* remainder (1+ remainder)) list-of-digits-for-solution)))
+	   	(reverse (cons (subseq *base62* remainder (1+ remainder)) list-of-digits-for-solution)))
 					; subseq gets the value of the digit in base62, it then adds the value to
 					; the list, the list keeps track of the digits of the whole number in base 62
 	  
