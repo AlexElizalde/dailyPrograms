0a585b80828ea2f1e244b6dcf76d4c054f15fd27
Alex <alexelizalde@my.unt.edu>
2018-05-21 12:25:29 -0500

    tested up to 39 916 800(without spaces) which yields 11./studio.sh doesn't work forthe factorial of 20

--- prog20161003.java
+++ prog20161003.java
@@ -3,19 +3,21 @@ import java.util.*;
 public class prog20161003
 {
 
-    public static void finder(int numToFind, int currentNum)
+    public static void finder(int numToFind, int currentNum, int lastNumInSeq)
     {
 	if(numToFind == 0)
 	    System.out.println(" = 1!");
 
-	else if(currentNum < numToFind)
-	    finder(numToFind, (currentNum * (currentNum + 1)));
+	else if(currentNum < numToFind) {
+	    System.out.println("numToFind: " + numToFind + "  currentNum:" + currentNum
+			       + " lastNumInSeq:" + lastNumInSeq);
+	    finder(numToFind, (currentNum * (lastNumInSeq + 1)), (lastNumInSeq + 1));   }
 
 	else if(currentNum == numToFind)
-	    System.out.println(" = " + currentNum + "!");
+	    System.out.println(" = " + lastNumInSeq + "!");
 
 	else if(currentNum > numToFind)
-	    System.out.println("\tNONE");
+	    System.out.println("\tNONE" + " " + currentNum + " " + lastNumInSeq);
 
 	else
 	    System.out.println("error! something screwed up!");
@@ -28,11 +30,11 @@ public class prog20161003
 	String  input    = keyboard.nextLine();
 	
 	//scan input numbers until the user just presses the enter key without
-	//an input number
+ 	//an input number
 	while(!(input.equals("")))
 	{
 	    
-	    finder( Integer.parseInt(input), 1);
+	    finder( Integer.parseInt(input), 1, 1);
 
 	    input = keyboard.nextLine();
 	}
